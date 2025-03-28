86a32f1e4b6f73a87761c6528ca0233f0a818425
CV SAMEEHANA <cvsameehana@gmail.com>
2025-03-28 12:21:40 +0530

    changed theme

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 
 
 export const useThemeStore = create((set) => ({
-    theme: localStorage.getItem("chat-theme") || "coffee",
+    theme: localStorage.getItem("chat-theme") || "forest",
     setTheme: (theme) => {
         localStorage.setItem("chat-theme", theme);
         set({theme});
